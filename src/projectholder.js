08fceba022b464443f1e3ec9cfe4a798e8480db8
Anurag <anuragagrawal4998@gmail.com>
2021-02-19 18:00:22 +0530

    add airline image

---
+++ src/projectholder.js
@@ -0,0 +1,69 @@
+
+ const project = [
+    {
+        "path" : "/images/airline.png",
+        "title" : "ASAY Airlines",
+        "description" : "Airline Reservation system built using ASP.NET MVC, frontend in angular",
+        "code":"https://github.com/anurag4998/Airline",
+        "demo":""
+    },
+    {
+        "path" : "/images/certsfolio1.png",
+        "title" : "Certsfolio",
+        "description" : "A one place stop to aggregate all your certificates (alpha)",
+        "code":"",
+        "demo":"https://certsfolio.netlify.app/"
+    },
+
+    {
+    "path" : "/images/weather.png",
+    "title" : "Weather API",
+    "description" : "A landing page concept for API provider, integrated with third party API to show demo",
+    "code":"https://github.com/anurag4998/Weatherapp",
+    "demo":"https://weatherapi19.netlify.app/"
+    },
+
+    {
+        "path" : "/images/covid.png",
+        "title" : "Covid Tracker",
+        "description" : "A Covid-19 tracker created using react JS, graphical representation of cases using Chart.Js.",
+        "code":"https://github.com/anurag4998/Covid19-Tracker",
+        "demo":"https://covidtracker19india.netlify.app/"
+    },
+
+    {
+        "path" : "/images/taskmanager.png",
+        "title" : "Task Manager",
+        "description" : "A MERN Stack based Taskmanager, Uses JWT for authetication.",
+        "code":"https://github.com/anurag4998/Task_manager--frontend",
+        "demo":"https://task-manager04.netlify.app/"
+    },
+    {
+        "path" : "/images/currency.png",
+        "title" : "Currency Convertor",
+        "description" : "A currency convertor app built using the free currency convertor API ",
+        "code":"https://github.com/anurag4998/Curreny-Convertor",
+        "demo":"https://currencyswap.netlify.app/"
+    },
+
+    {
+        "path" : "/images/burger.png",
+        "title" : "Burger.INC",
+        "description" : "A parallax scrolling landing page concept built using scss",
+        "code":"https://github.com/anurag4998/Burger.inc",
+        "demo":"https://burgerinc.netlify.app/"
+    },
+
+    {
+        "path" : "/images/bookmark.png",
+        "title" : "Bookmark",
+        "description" : "A landing page concept built using scss",
+        "code":"https://github.com/anurag4998/Bookmark",
+        "demo":"https://bookmarkland.netlify.app/"
+    },
+   
+
+
+]
+
+export default project
