4e195c38d1be9b219060230d65a57bdfa7456354
Anurag <anuragagrawal4998@gmail.com>
2021-02-19 14:53:37 +0530

    add bootstrap.min.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../src/styles/bootstrap.min.css'
 import "./styles/styles.scss";
 ReactDOM.render(
   <React.StrictMode>
