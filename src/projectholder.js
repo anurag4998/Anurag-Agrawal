77916d3f4881d4582dcd798f2d8ff9d0e892958b
Anurag <anuragagrawal4998@gmail.com>
2021-02-20 20:35:27 +0530

    update projectholder.js update json

--- src/projectholder.js
+++ src/projectholder.js
@@ -1,18 +1,12 @@
 
  const project = [
     {
-        "path" : "/images/airline.png",
-        "title" : "ASAY Airlines",
-        "description" : "Airline Reservation system built using ASP.NET MVC, frontend in angular",
-        "code":"https://github.com/anurag4998/Airline",
-        "demo":""
-    },
-    {
         "path" : "/images/certsfolio1.png",
         "title" : "Certsfolio",
         "description" : "A one place stop to aggregate all your certificates (alpha)",
         "code":"",
-        "demo":"https://certsfolio.netlify.app/"
+        "demo":"https://certsfolio.netlify.app/",
+        "pill" : "MERN"
     },
 
     {
@@ -20,7 +14,8 @@
     "title" : "Weather API",
     "description" : "A landing page concept for API provider, integrated with third party API to show demo",
     "code":"https://github.com/anurag4998/Weatherapp",
-    "demo":"https://weatherapi19.netlify.app/"
+    "demo":"https://weatherapi19.netlify.app/",
+    "pill" : "React"
     },
 
     {
@@ -28,22 +23,37 @@
         "title" : "Covid Tracker",
         "description" : "A Covid-19 tracker created using react JS, graphical representation of cases using Chart.Js.",
         "code":"https://github.com/anurag4998/Covid19-Tracker",
-        "demo":"https://covidtracker19india.netlify.app/"
+        "demo":"https://covidtracker19india.netlify.app/",
+        "pill" : "React"
+
     },
 
     {
         "path" : "/images/taskmanager.png",
         "title" : "Task Manager",
-        "description" : "A MERN Stack based Taskmanager, Uses JWT for authetication.",
+        "description" : "A MERN Stack based Taskmanager app, Uses JWT for authetication.",
         "code":"https://github.com/anurag4998/Task_manager--frontend",
-        "demo":"https://task-manager04.netlify.app/"
+        "demo":"https://task-manager04.netlify.app/",
+        "pill" : "MERN"
+
+    },
+    {
+        "path" : "/images/weatherexpress.png",
+        "title" : "Weather App",
+        "description" : "A simple weather app built using node and Express ",
+        "code":"https://github.com/anurag4998/weather_express",
+        "demo":"https://aaru-weather-application49.herokuapp.com/",
+        "pill" : "Node JS"
+
     },
     {
         "path" : "/images/currency.png",
         "title" : "Currency Convertor",
         "description" : "A currency convertor app built using the free currency convertor API ",
         "code":"https://github.com/anurag4998/Curreny-Convertor",
-        "demo":"https://currencyswap.netlify.app/"
+        "demo":"https://currencyswap.netlify.app/",
+        "pill" : "React"
+
     },
 
     {
@@ -51,15 +61,19 @@
         "title" : "Burger.INC",
         "description" : "A parallax scrolling landing page concept built using scss",
         "code":"https://github.com/anurag4998/Burger.inc",
-        "demo":"https://burgerinc.netlify.app/"
+        "demo":"https://burgerinc.netlify.app/",
+        "pill" : "SCSS"
+
     },
 
     {
         "path" : "/images/bookmark.png",
         "title" : "Bookmark",
-        "description" : "A landing page concept built using scss",
+        "description" : "A landing page concept built using flexbox and scss design pattern",
         "code":"https://github.com/anurag4998/Bookmark",
-        "demo":"https://bookmarkland.netlify.app/"
+        "demo":"https://bookmarkland.netlify.app/",
+        "pill" : "SCSS"
+
     },
    
 
