0c6de94d8da495a7cc8f8416150c725b2ccc3f61
Anurag <anuragagrawal4998@gmail.com>
2021-02-20 11:55:15 +0530

    update footer.js add heading

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
     return(
         <div className = 'footer'>
             <div className ='container'>
+                <h2 className = 'footer__heading'>Let's Connect </h2>
                 <div className ='social-links'>
                     <a href = "https://github.com/anurag4998" target="_blank"><i className="devicon-github-original"></i></a>
                     <a href = "https://twitter.com/Anurag4998" target="_blank"> <i className="devicon-twitter-original"></i></a>
