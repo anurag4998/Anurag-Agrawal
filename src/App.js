9085fec01e4a1462971d5a4d146cc1e31dee28e4
Anurag <anuragagrawal4998@gmail.com>
2021-02-20 20:36:27 +0530

    update App.js add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from '../src/components/header'
+import Skills from '../src/components/skills'
+import Intro from '../src/components/aboutme'
+import Navbar from '../src/components/navbar'
+import Projects from '../src/components/projects'
+import Footer from '../src/components/footer'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Header/>
+      <Projects/>
+      <Intro/>
+      <Footer/>
     </div>
   );
 }
